285462eda61aca5607e0ab7765eb2526cd7d6689
naidine <villeroznaidine19@gmail.com>
2023-05-04 18:33:20 +0800

    Add project files.

---
+++ Exam_villeroz_1/Program.cs
@@ -0,0 +1,54 @@
+﻿
+class Car
+{
+    public string Brand { get; set; }
+    public int Price { get; set; }
+
+    public Car(string brand, int price)
+    {
+        Brand = brand;
+        Price = price;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Car> cars = new List<Car>();
+        cars.Add(new Car("Honda", 1150000));
+        cars.Add(new Car("Toyota", 1250000));
+        cars.Add(new Car("Geely", 1300000));
+        cars.Add(new Car("Nissan", 1500000));
+        cars.Add(new Car("Ford", 2500000));
+        cars.Add(new Car("Kia", 1100000));
+        cars.Add(new Car("Chevrolet", 1800000));
+        cars.Add(new Car("Hyundai", 1600000));
+        cars.Add(new Car("Subaru", 2000000));
+        cars.Add(new Car("Mitsubishi", 1400000));
+
+
+        var filteredCars = cars.Where(car => car.Price >= 1000000);
+
+        var groupedCars = filteredCars.GroupBy(car => car.Brand);
+
+        foreach (var group in groupedCars)
+        {
+            Console.WriteLine("=== " + group.Key + " ===");
+
+            foreach (var car in group)
+            {
+                if (car.Price > 1200000)
+                {
+                    Console.WriteLine(car.Brand + " - this belongs to class A car");
+                }
+                else
+                {
+                    Console.WriteLine(car.Brand + " - this belongs to class B car");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
